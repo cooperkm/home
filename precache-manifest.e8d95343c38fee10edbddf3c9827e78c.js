664f0344a7d0820e3d276bdcaf60e3e73c96739f
kylecooper <kyle.cooper@morgan6.com>
2020-07-30 18:30:37 -0400

    Updates

---
+++ precache-manifest.e8d95343c38fee10edbddf3c9827e78c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff5c87f2e9bc63f7a14d451831043b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "07e9848b1ed8d1761b37",
+    "url": "/static/css/main.0b5b9f12.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e9848b1ed8d1761b37",
+    "url": "/static/js/main.a1b69041.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "0f24d8341a9b92f8b0568d5c7db1cc08",
+    "url": "/static/media/homeBackground.0f24d834.jpg"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/static/media/resume.0c246fa0.pdf"
+  }
+]);
